57034a144d985279f42be01be2c2757c4e5d1348
junzero741 <junzero741@gmail.com>
2021-04-18 14:57:18 +0900

    [Delete]: comment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link,
-//   useRouteMatch,
-//   useParams,
-// } from 'react-router-dom';
 import Header from 'components/Header';
 import Nav from 'components/Nav';
 
@@ -19,38 +11,6 @@ function App() {
     </>
   );
 }
-
-// const Topics = () => {
-//   let match = useRouteMatch();
-
-//   return (
-//     <div>
-//       <h2>Topics</h2>
-//       <ul>
-//         <li>
-//           <Link to={`${match.url}/components`}>Components</Link>
-//         </li>
-//         <li>
-//           <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-//         </li>
-//       </ul>
-
-//       <Switch>
-//         <Route path={`${match.path}/:topicId`}>
-//           <Topic />
-//         </Route>
-//         <Route path={match.path}>
-//           <h3>Please select a topic.</h3>
-//         </Route>
-//       </Switch>
-//     </div>
-//   );
-// };
-
-// const Topic = () => {
-//   let { topicId } = useParams();
-//   return <h3>Requested topic ID: {topicId}</h3>;
-// };
 
 export default App;
 
