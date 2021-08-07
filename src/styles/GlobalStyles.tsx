c3b0a47254c07589a0491fcb17727659bd90ac56
junzero741 <junzero741@gmail.com>
2021-08-07 15:24:32 +0900

    WIP: 노토산스 폰트 적용중

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,9 +1,51 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import {NotoSansKR_Black,
+	NotoSansKR_Bold,
+	NotoSansKR_Regular,
+	NotoSansKR_Medium,
+	NotoSansKR_Light,
+	NotoSansKR_Thin,} from '../fonts/font';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
   
+  body {
+	font-family:'NotoSansKR_Black';
+    font-size: 16px;
+    color: #2e6399;
+	font-weight: 900;
+  }
+  
+  @font-face {
+  font-family: "NotoSansKR_Black";
+  src: local('NotoSansKR_Black'), url(${NotoSansKR_Black})format("woff");
+	}
+
+  @font-face {
+  font-family: "NotoSansKR_Bold";
+  src: local('NotoSansKR_Bold'), url(${NotoSansKR_Bold})format("woff");
+	}
+
+	@font-face {
+  font-family: "NotoSansKR_Regular";
+  src: local('NotoSansKR_Regular'), url(${NotoSansKR_Regular})format("woff");
+	}
+
+	@font-face {
+  font-family: "NotoSansKR_Medium";
+  src: local('NotoSansKR_Medium'), url(${NotoSansKR_Medium})format("woff");
+	}
+
+	@font-face {
+  font-family: "NotoSansKR_Light";
+  src: local('NotoSansKR_Light'), url(${NotoSansKR_Light})format("woff");
+	}
+
+	@font-face {
+  font-family: "NotoSansKR_Thin";
+  src: local('NotoSansKR_Thin'), url(${NotoSansKR_Thin})format("woff");
+	}
   
 `;
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
 					transpileOnly: true,
 				},
 			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "fonts/",
+						},
+					},
+				],
+			},
 		],
 	},
 	plugins: [new ForkTsCheckerWebpackPlugin()],
